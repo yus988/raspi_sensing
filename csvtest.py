0b032c4802359fb528eb5ab558e8a3e09afd3e98
Yusuke Yamazaki <z.yusuke.yamazaki@gree.net>
2020-12-07 01:45:12 +0900

    test csv

--- csvtest.py
+++ csvtest.py
@@ -1,24 +1,25 @@
 # -*- coding: utf-8 -*-
-
 import datetime
 import csv
 
 dt_now = datetime.datetime.now()
-filename = './csv/' + dt_now.strftime('%m-%d_%H-%M-%S') + '.csv'
-print(dt_now.strftime('%m%d_%H-%M-%S'))
+filename = "./csv/" + dt_now.strftime("%m-%d_%H-%M-%S") + ".csv"
+print(dt_now.strftime("%m%d_%H-%M-%S"))
 
 # ファイルオープン
-f = open(filename, 'w')
-writer = csv.writer(f, lineterminator='\n')
+f = open(filename, "w")
+writer = csv.writer(f, lineterminator="\n")
 
 # データをリストに保持
 csvlist = []
 # csvlist.append("hoge")
 # csvlist.append("fuga")
 
-csvlist.extend([2,3,4])
+csvlist.append([2,3,4])
+csvlist.append([2,4,5])
+
 # 出力
-writer.writerow(csvlist)
+writer.writerows(csvlist)
 
 # ファイルクローズ
 f.close()
