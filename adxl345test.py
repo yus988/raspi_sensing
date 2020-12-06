333988df70543eb8f01c53ec66eba99f8c726387
Yusuke Yamazaki <z.yusuke.yamazaki@gree.net>
2020-12-07 00:16:12 +0900

    update adxl345 csv & constructre

--- adxl345test.py
+++ adxl345test.py
@@ -5,124 +5,97 @@ import RPi.GPIO as GPIO
 import os
 import smbus
 import time
+from time import sleep
+from time import perf_counter
+import matplotlib.pyplot as plt
+import csv
+import datetime
+
+
+# DevAdr = 0x1d
+# DevAdr = 0x53
+
 
 # ADXL345 Class
 class ADXL345:
-    DevAdr = 0x1d
+    def __init__(self, addr):
+        self.DevAdr = addr
+
     myBus = ""
-    if GPIO.RPI_INFO['P1_REVISION'] == 1:
+    if GPIO.RPI_INFO["P1_REVISION"] == 1:
         myBus = 0
     else:
         myBus = 1
-        
+
     b = smbus.SMBus(myBus)
 
     def setUp(self):
-        self.b.write_byte_data(self.DevAdr, 0x2C, 0x0B) # BandwidthRate
-        self.b.write_byte_data(self.DevAdr, 0x31, 0x00) # DATA_FORMAT 10bit 2g
-        self.b.write_byte_data(self.DevAdr, 0x38, 0x00) # FIFO_CTL OFF
-        self.b.write_byte_data(self.DevAdr, 0x2D, 0x08) # POWER_CTL Enable
+        self.b.write_byte_data(self.DevAdr, 0x2C, 0x0B)  # BandwidthRate
+        self.b.write_byte_data(self.DevAdr, 0x31, 0x00)  # DATA_FORMAT 10bit 2g
+        self.b.write_byte_data(self.DevAdr, 0x38, 0x00)  # FIFO_CTL OFF
+        self.b.write_byte_data(self.DevAdr, 0x2D, 0x08)  # POWER_CTL Enable
 
-    def getValueX(self):
-        return self.getValue(0x32)
-
-    def getValueY(self):
-        return self.getValue(0x34)
-
-    def getValueZ(self):
-        return self.getValue(0x36)
+    def getAccValue(self):
+        x = self.getValue(0x32)
+        y = self.getValue(0x34)
+        z = self.getValue(0x36)
+        return [x, y, z]
 
     def getValue(self, adr):
-        tmp = self.b.read_byte_data(self.DevAdr, adr+1)
+        tmp = self.b.read_byte_data(self.DevAdr, adr + 1)
         sign = tmp & 0x80
         tmp = tmp & 0x7F
-        tmp = tmp<<8
+        tmp = tmp << 8
         tmp = tmp | self.b.read_byte_data(self.DevAdr, adr)
-#       print '%4x' % tmp # debug
+        #       print '%4x' % tmp # debug
 
         if sign > 0:
             tmp = tmp - 32768
 
         return tmp
-
-
-class ADXL345_2:
-    DevAdr = 0x53
-    
-    myBus = ""
-    if GPIO.RPI_INFO['P1_REVISION'] == 1:
-        myBus = 0
-    else:
-        myBus = 1
-        
-    b = smbus.SMBus(myBus)
-
-    def setUp(self):
-        self.b.write_byte_data(self.DevAdr, 0x2C, 0x0B) # BandwidthRate
-        self.b.write_byte_data(self.DevAdr, 0x31, 0x00) # DATA_FORMAT 10bit 2g
-        self.b.write_byte_data(self.DevAdr, 0x38, 0x00) # FIFO_CTL OFF
-        self.b.write_byte_data(self.DevAdr, 0x2D, 0x08) # POWER_CTL Enable
-
-    def getValueX(self):
-        return self.getValue(0x32)
-
-    def getValueY(self):
-        return self.getValue(0x34)
-
-    def getValueZ(self):
-        return self.getValue(0x36)
-
-    def getValue(self, adr):
-        tmp = self.b.read_byte_data(self.DevAdr, adr+1)
-        sign = tmp & 0x80
-        tmp = tmp & 0x7F
-        tmp = tmp<<8
-        tmp = tmp | self.b.read_byte_data(self.DevAdr, adr)
-#       print '%4x' % tmp # debug
-
-        if sign > 0:
-            tmp = tmp - 32768
-
-        return tmp
-#   tmp = self.b.read_word_data(self.DevAdr, adr)
-
-# with open('test.csv', 'w') as f:
-#     writer = csv.writer(f)
-#     writer.writerow([0, 1, 2])
-#     writer.writerow(['a', 'b', 'c'])
 
 # MAIN
-myADXL345 = ADXL345()
-myADXL345.setUp()
+def main():
 
-myADXL345_2 = ADXL345_2()
-myADXL345_2.setUp()
+    # 書き込むファイルの作成
+    dt_now = datetime.datetime.now()
+    filename = './csv/' + dt_now.strftime('%m-%d_%H-%M-%S') + '.csv'
+    print(dt_now.strftime('%m%d_%H-%M-%S'))
 
-# LOOP
-for a in range(1000):
+    # ファイルオープン
+    f = open(filename, 'w')
+    writer = csv.writer(f, lineterminator='\n')
+    t_init = perf_counter()
 
-    print(time.time())
+    myADXL345_1 = ADXL345(0x1D)
+    myADXL345_1.setUp()
 
-    x = myADXL345.getValueX()
-    y = myADXL345.getValueY()
-    z = myADXL345.getValueZ()
+    myADXL345_2 = ADXL345(0x53)
+    myADXL345_2.setUp()
+
+    file1 = open("myADXL345_1.csv", "w")
+    file2 = open("myADXL345_2.csv", "w")
+
+    # LOOP
+    while perf_counter() - t_init < 10.0:
+
+        print(time.time())
+        x, y, z = myADXL345_1.getAccValue()
+        print("X=", x, "Y=", y, "Z=", z)
+        csvlist = []
+        csvlist.extend(datetime.isoformat()),x,y,z)
+        writer.writerow(csvlist)
+
+        x2, y2, z2 = myADXL345_2.getAccValue()
+        # print(time.time())
+        print("X2=", x2, "Y2=", y2, "Z2=", z2)
+
+        csvlist = []
+        csvlist.extend(datetime.isoformat()),x,y,z)
+        writer.writerow(csvlist)
     
+    f.close()
 
-    print("X=", x)
-    # print(time.time())
-    print("Y=", y)
-    # print(time.time())
-    print("Z=", z)
-    
-    x2 = myADXL345_2.getValueX()
-    y2 = myADXL345_2.getValueY()
-    z2 = myADXL345_2.getValueZ()
-    
-    # print(time.time())
-    print("X2=", x2)
-    print("Y2=", y2)
-    print("Z2=", z2)
-  
-    # os.system("clear")
-    # time.sleep(1)
 
+if __name__ == "__main__":
+    main()
